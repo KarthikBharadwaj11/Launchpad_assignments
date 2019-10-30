0c3892a3f6635716390b5a1cb0a77d205a8a4c70
KarthikBharadwaj11 <57124042+KarthikBharadwaj11@users.noreply.github.com>
2019-10-30 22:30:52 +0530

    Add files via upload

---
+++ Program6.py
@@ -0,0 +1,15 @@
+n=int(input('Enter the number of students:'))
+all_students=[]
+for i in range(0,n):
+    name=input("Enter the name of student:")
+    print(name)
+    usn=input("Enter the roll number of student:")
+    print(usn)
+dict={'Name':name, 'USN':usn}
+print(dict)
+
+
+    
+   
+        
+    
