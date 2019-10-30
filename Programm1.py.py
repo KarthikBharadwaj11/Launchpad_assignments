7d3e4e0e8e8b2dccd797ebcf2f6be4e69d5bfc05
KarthikBharadwaj11 <57124042+KarthikBharadwaj11@users.noreply.github.com>
2019-10-30 22:44:56 +0530

    Add files via upload

---
+++ Programm1.py.py
@@ -0,0 +1,8 @@
+name= input("Enter your name:")
+age=input("Enter your age:")
+birthyear= 2019-int(age)
+hundredyear=int(birthyear)+int(100)
+num= int(hundredyear)
+print(f"Hey {name}!You will turn 100 years old in {num}")
+
+    
